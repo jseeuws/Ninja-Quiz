b1db8f95025cdc75e9fe199372087ecbe291c0b4
jennis <jennis.seeuws@hotmail.com>
2020-04-04 15:39:25 +0200

    scroll to method

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -13,7 +14,8 @@ form.addEventListener('submit', e => {
             score += 25;
         }
     });
-
-    console.log(score);
-
+    
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%` ;
+    result.classList.remove('d-none');
 });
